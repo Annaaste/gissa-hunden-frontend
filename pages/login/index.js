68fe968cf125d547956c652bf5664f1ffa5eaa67
Annaaste <92034378+Annaaste@users.noreply.github.com>
2023-08-12 16:56:33 +0200

    fix simple login/signup

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,9 +1,16 @@
 import { useState } from 'react';
 import axios from 'axios';
+import styles from './login.module.scss';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  const router = useRouter();
 
   const handleLogin = async () => {
     try {
@@ -11,18 +18,47 @@ export default function Login() {
       const token = response.data.token;
       localStorage.setItem('jwtToken', token); 
 
-      // Redirect or perform other actions on successful login
+      setSuccess(true);
+      router.push('/add-dog');
     } catch (error) {
-      // Handle login failure
+      
+      setError('Felaktig inloggning, försök igen.');
+      setEmail('');
+      setPassword('');
     }
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={handleLogin}>Login</button>
+  <>
+  <div>
+    <h1>Gissa Hunden</h1>
+  </div>
+    <div className={styles.loginContainer}>
+      <h2 className={styles.loginTitle}>Inloggning</h2>
+      {error && <div className={styles.error}>{error}</div>}
+      <input
+        type="email"
+        placeholder="Email"
+        className={styles.loginInput}
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        className={styles.loginInput}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button className={styles.loginButton} onClick={handleLogin}>
+        Logga in
+      </button>
+
+      <div>
+        <p>Ny på Gissa Hunden?</p>
+        <Link href='/signup'>Skapa konto här!</Link>
+      </div>
     </div>
+    </>
   );
 }
--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,11 +1,19 @@
 import { useState } from 'react';
 import axios from 'axios';
+import styles from '../login/login.module.scss';
+import { useRouter } from 'next/router';
+
 
 export default function SignupPage() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+  
+  const router = useRouter();
+
 
   const handleSignup = async (e) => {
     e.preventDefault(); 
@@ -18,24 +26,59 @@ export default function SignupPage() {
       });
       const token = response.data.token;
       localStorage.setItem('jwtToken', token);
-
-      // Redirect or perform other actions on successful signup
+      setSuccess(true);
+      
+      router.push('/login')
     } catch (error) {
-      // Handle signup failure
+      setError('Felaktig inloggning, försök igen.');
+      setEmail('');
+      setPassword('');
+      setFirstName('');
+      setLastName('');
     }
   };
 
   return (
+    <>
     <div>
-      <h2>Signup</h2>
+    <h1>Gissa Hunden</h1>
+    </div>
+    <div className={styles.loginContainer}>
+      <h2 className={styles.loginTitle} >Skapa konto</h2>
+      {error && <div className={styles.error}>{error}</div>}
       <form onSubmit={handleSignup}>
-        <input type="text" placeholder="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-        <input type="text" placeholder="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <button type="submit">Signup</button>
+        <input 
+          type="text" 
+          placeholder="First Name" 
+          value={firstName} 
+          className={styles.loginInput}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <input 
+          type="text" 
+          placeholder="Last Name" 
+          value={lastName} 
+          className={styles.loginInput}
+          onChange={(e) => setLastName(e.target.value)} 
+        />
+        <input 
+          type="email" 
+          placeholder="Email" 
+          value={email} 
+          className={styles.loginInput}
+          onChange={(e) => setEmail(e.target.value)} 
+        />
+        <input 
+          type="password" 
+          placeholder="Password" 
+          value={password} 
+          className={styles.loginInput}
+          onChange={(e) => setPassword(e.target.value)} 
+        />
+        <button className={styles.loginButton} type="submit">Skapa konto</button>
       </form>
     </div>
+    </>
   );
 }
 
